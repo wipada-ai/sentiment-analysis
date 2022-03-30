e8cc6baf90fb022195208c824b4b2e3368ec3da4
wipada-ai <62405562+wipada-ai@users.noreply.github.com>
2022-03-30 20:44:43 +0700

    Add files via upload

---
+++ flask sentiment analysis/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import numpy as np
+import joblib
+import re
+import string
+import pythainlp
+from pythainlp import word_tokenize, Tokenizer
+import pickle
+
+# Declare a Flask app
+app = Flask(__name__) 
+
+@app.route('/')
+def home():
+    return render_template('website.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    # If a form is submitted
+    if request.method == "POST":
+        
+        # Unpickle classifier
+        pipeline = joblib.load("pipeline.pkl")
+
+        # load the model from disk
+        customTokenizer = pickle.load(open("customTokenizer.pkl", 'rb'))
+        
+        newtext = request.form['text']
+
+        # Tokenize 
+        def LabelText(newText):
+            tokenizer = customTokenizer.word_tokenize(newText)
+
+            tokenizer = ','.join(map(str, tokenizer))
+
+            tokenizer = [re.sub(r',',' ',tokenizer)]
+
+            return tokenizer
+            
+        # Get prediction
+        my_predict = pipeline.predict(LabelText(newtext))
+
+    else:
+        my_predict = ""
+
+    return render_template("website.html", prediction = my_predict)
+
+# Running the app
+if __name__ == '__main__':
+    app.run(debug = True)
+
+
+
